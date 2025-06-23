19a87d43344952d815f68bc154c4c7871b3015e0
Varun Selvam <varuns71100@gmail.com>
2025-06-22 18:55:14 -0600

    Update official_wordle_function.py
    Changed variable names

--- official_wordle_function.py
+++ official_wordle_function.py
@@ -17,17 +17,17 @@ def wordle(word,guess):
             total_word_cnt = word.count(guess_list[i][0])
             processed_words.append(guess_list[i])
             difference = total_word_cnt - (correct_pos + wrong_pos)
-            a = guess_list[i]
+            letter = guess_list[i]
             if difference != 0:
                 index_position = []
                 abs_diff = abs(difference)
                 # print("statement passed difference != 0")
-                for j in range(0,len(guess_list)):
-                    if a == guess_list[j]:
-                        index_position.append(j)
+                for location in range(0,len(guess_list)):
+                    if letter == guess_list[location]:
+                        index_position.append(location)
                         # print("Appending Letter")
-                for k in range(0,abs_diff):
-                    m = index_position[k]
-                    guess_list[m] = guess_list[m][0] + "**"
+                for letter_loc in range(0,abs_diff):
+                    le = index_position[letter_loc]
+                    guess_list[le] = guess_list[le][0] + "**"
 
     return guess_list
